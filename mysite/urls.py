9b9d87d6f18cafb8baf04673eb9978811b8f4137
kimimaji <kojikojibo@hotmail.com>
2021-08-04 13:08:47 +0900

    Added 1st index.html

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+#custom
+admin.site.site_title = 'KGB' 
+admin.site.site_header = 'ちくま精機　開発技術部' 
+admin.site.index_title = '管理メニュー'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('BestPrice.urls')),
 ]
